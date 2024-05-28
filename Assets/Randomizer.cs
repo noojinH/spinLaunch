c43954f507092ca883cd095d8e6d065016e366e9
NeebaCanning <dunillroud@gmail.com>
2024-05-28 19:27:13 +0900

    Update Randomizer.cs
    making Save SYSTEM

--- Assets/Randomizer.cs
+++ Assets/Randomizer.cs
@@ -26,17 +26,27 @@ public class Randomizer : MonoBehaviour
 
             if (targetScript0 != null)
             {
+                if(PlayerPrefs.GetInt("attempt") != null){
+                PlayerPrefs.SetInt("attempt", PlayerPrefs.GetInt("attempt") + 1);
+                }
+                else{
+                    PlayersPrefs.SetInt("attempt", 1);
+                }
                 targetScript0.maxRPM = Random.Range(300f, 3000f);
                 tmp0.text =  targetScript0.maxRPM.ToString() + "RPM";
+                PlayerPrefs.SetFloat("maxRPMvar", targetScript0.maxRPM);
 
                 targetScript0.maxRPMTime = Random.Range(3f, 6f);
                 tmp0.text +=  " in " + targetScript0.maxRPMTime.ToString() +"s";
+                PlayerPrefs.SetFloat("maxTimevar", targetScript0.maxRPMTime);
 
                 rocket0.GetComponent<Rigidbody>().mass = Random.Range(0.5f, 1.5f);
                 tmp0.text += "\n" + rocket0.GetComponent<Rigidbody>().mass.ToString() + "kg";
+                PlayerPrefs.SetFloat("massVar", rocket0.GetComponent<Rigidbody>().mass);
 
                 rocket0.GetComponent<Attacher>().delay = Random.Range(1f, 7f);
                 tmp0.text += " \nlaunch: " + rocket0.GetComponent<Attacher>().delay.ToString()+"seconds";
+                PlayerPrefs.SetFloat("delayVar", rocket0.GetComponent<Attacher>().delay);
 
 
                 //Debug.Log("Randomized value: " + targetScript.maxRPM);
