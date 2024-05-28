c9a23160b4ade5b69987696e41d3d98a1ecce042
NeebaCanning <dunillroud@gmail.com>
2024-05-28 23:50:45 +0900

    scatter with GPT

--- Assets/scatterPlot.cs
+++ Assets/scatterPlot.cs
@@ -1,21 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+
+[System.Serializable]
+public class PointData{
+    public float x0;
+    public float y0;
+    public float z0;
+    public float add1;
+    public float add2;
+
+    public PointData(float x, float y, float z, float ad1, float ad2)
+    {
+        this.x0 = x;
+        this.y0 = y;
+        this.z0 = z;
+        this.add1 = ad1;
+        this.add2 = ad2;
+    }
+}
 
 public class scatterPlot : MonoBehaviour
 {
     public GameObject success;
     public GameObject fail;
     [SerializeField]
-    private float[] rnv;
+    List<PointData> rnv = new List<PointData>();
+
+    public void Load(){
+        rnv.Clear();
+        for(int _ = 1; _ < PlayerPrefs.GetInt("attempt") + 1; _++){
+        float x0 = PlayerPrefs.GetFloat("maxRPMvar" + _.ToString());
+        float y0 = PlayerPrefs.GetFloat("maxTimevar" + _.ToString());
+        float z0 = PlayerPrefs.GetFloat("delayVar" +  _.ToString());
+        float add1 = PlayerPrefs.GetFloat("massVar" +  _.ToString());
+        float add2 = PlayerPrefs.GetFloat("record" +  _.ToString());
+
+        PointData newPoint = new PointData(x0, y0, z0, add1, add2);
+        rnv.Add(newPoint);
+    }
+
+        for (int i = 0; i < rnv.Count; i++)
+        {
+            PlayerPrefs.SetFloat("point" + i + "_x", rnv[i].x0);
+            PlayerPrefs.SetFloat("point" + i + "_y", rnv[i].y0);
+            PlayerPrefs.SetFloat("point" + i + "_z", rnv[i].z0);
+            PlayerPrefs.SetFloat("point" + i + "_ad1", rnv[i].add1);
+            PlayerPrefs.SetFloat("point" + i + "_ad2", rnv[i].add2);
+        }
+        PlayerPrefs.SetInt("pointCount", rnv.Count);
+    }
 
     void Start()
     {
-        rnv = new float[4];
-        rnv[1] = PlayerPrefs.GetFloat("maxRPMvar" + null);
-        rnv[2] = PlayerPrefs.GetFloat("maxTimeVar" + null);
-        rnv[3] = PlayerPrefs.GetFloat("delayVar" + null);
-        rnv[4] = PlayerPrefs.GetFloat("massVar" + null);
-        rnv[5] = PlayerPrefs.GetFloat("record" + null);
+       int pointCount = PlayerPrefs.GetInt("pointCount", 0);
+       
+       for (int i = 0; i < pointCount; i++)
+        {
+            float x0 = PlayerPrefs.GetFloat("point" + i + "_x");
+            float y0 = PlayerPrefs.GetFloat("point" + i + "_y");
+            float z0 = PlayerPrefs.GetFloat("point" + i + "_z");
+            float add1 = PlayerPrefs.GetFloat("point" + i + "_ad1");
+            float add2 = PlayerPrefs.GetFloat("point" + i + "_ad2");
+
+            PointData loadedPoint = new PointData(x0, y0, z0, add1, add2);
+            rnv.Add(loadedPoint);
     }
+}
+
+public void back(){
+    StartCoroutine(toHome());
+}
+
+IEnumerator toHome(){
+    yield return new WaitForEndOfFrame();
+    SceneManager.LoadScene(0);
+}
 }
